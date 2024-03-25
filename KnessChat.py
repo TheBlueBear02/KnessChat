98730f67309b3d673c02dd18fae79e98a5be37ba
Amir <amir.gabbay@gmail.com>
2024-03-25 23:32:11 +0200

    added Roles to users

--- KnessChat.py
+++ KnessChat.py
@@ -6,7 +6,10 @@ def display_user_message(tweet,all_tweets):
     # Define styles for user1
     id = tweet["UserId"]
     profile = next((p for p in all_tweets["Members"] if p["Id"] == id), None)
-    
+    hasRole = ""
+    if profile["additional_role"] != "":
+        hasRole = "|"
+
     if profile["is_coalition"] == True: # Define styles for coalition or other users
         alignment = "flex-end"
         background_color = "#DEEFFF"
@@ -28,12 +31,12 @@ def display_user_message(tweet,all_tweets):
     # Custom styling for the message bubble
     bubble = f"""
     <div style="display: flex; flex-direction: column; align-items: {alignment}; margin-top: 4px;">
-        <div style="max-width: 70%; margin: 5px; padding: 10px; background-color: {background_color}; border-radius: {border_radius}; color: {text_color}; text-align: right">
+        <div style="max-width: 80%; margin: 5px; padding: 10px; background-color: {background_color}; border-radius: {border_radius}; color: {text_color}; text-align: right">
             <table style="direction: {table};">
                 <tr style="border: none;"> 
                     <th style="border: none; text-align: {halign};">
                         <h5 style="margin:0; margin-left: 40px; padding: 0;">{profile["name"]}</h5>
-                        <i style="margin-left: 0px;">{profile["party"]}</i>
+                        <i style="margin-left: 0px;">{profile["additional_role"]} {hasRole} {profile["party"]}</i>
                     </th>
                     <th style="border: none;float: right;margin:0; padding: 0;">
                         <img style=" padding:0; margin:0; width:40px; height:40px; border-radius: 50%;" src="{profile["image"]}">
@@ -41,8 +44,8 @@ def display_user_message(tweet,all_tweets):
                 </tr>
                 <tr style="border: none;">
                     <td style="border: none;">
-                    <p dir= "rtl" style="margin-top: 10px; color: {text_color};">{tweet["Text"]}</p>
-                    <p style="color: {text_color}; text-align: {talign}; font-size: small;">{tweet["Time"]}</p></td>
+                    <p dir= "rtl" style="margin-top: 10px; color: {text_color}; margin:0px; padding:0px;">{tweet["Text"]}</p>
+                    <p style="color: {text_color}; text-align: {talign}; font-size: small; margin:0px; padding:0px;">{tweet["Time"]}</p></td>
                     <td style="border: none;"></td>
     </div>
     """
